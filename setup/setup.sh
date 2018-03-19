b436cff2da1420a6d2883df8cbdcccc08a10b280
Chris Truncer <CTruncer@christophertruncer.com>
2018-03-19 13:46:13 -0600

    Fixes issue #175

--- setup/setup.sh
+++ setup/setup.sh
@@ -78,7 +78,7 @@ func_check_env(){
   fi
 
   # Double check install
-  if [ "${os}" != "kali" ] || [ "${os}" == "parrot" ]; then
+  if [ "${os}" != "kali" ] || [ "${os}" == "parrot" ] && [ "${silent}" == false ]; then
     echo -e "\n ${BOLD}[!] NON-KALI Users: Before you begin the install, make sure that you have"
     echo -e "     the Metasploit-Framework installed before you proceed!${RESET}\n"
     read -p 'Continue? ([Y]/[n]o): ' installveil
