af14559ab94d1be81a87f85b0dd63619ccda683a
g0tmi1k <have.you.g0tmi1k@gmail.com>
2018-04-10 15:49:24 +0100

    Convert update-config.py to python3

--- config/update-config.py
+++ config/update-config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 """
 Take an options dictionary and update /etc/veil/settings.py
@@ -26,12 +26,12 @@ def generateConfig(options):
 #
 #################################################
 """
-    print "\n Veil Configuration:"
+    print( "\n Veil Configuration:" )
 
     # OS
     config += '# OS to use (Kali/Backtrack/Debian/Windows)\n'
     config += 'OPERATING_SYSTEM="' + options['OPERATING_SYSTEM'] + '"\n\n'
-    print " [*] OPERATING_SYSTEM = " + options['OPERATING_SYSTEM']
+    print( " [*] OPERATING_SYSTEM = " + options['OPERATING_SYSTEM'] )
 
     # OS (Linux)
     config += '# Specific Linux distro\n'
@@ -45,37 +45,37 @@ def generateConfig(options):
     # Terminal clearing
     config += '# Terminal clearing method to use (use "false" to disable it)\n'
     config += 'TERMINAL_CLEAR="' + options['TERMINAL_CLEAR'] + '"\n\n'
-    print " [*] TERMINAL_CLEAR = " + options['TERMINAL_CLEAR']
+    print( " [*] TERMINAL_CLEAR = " + options['TERMINAL_CLEAR'] )
 
     # Wine
     config += '# Wine environment\n'
     config += 'WINEPREFIX="' + options["WINEPREFIX"] + '"\n\n'
-    print " [*] WINEPREFIX = " + options["WINEPREFIX"]
+    print( " [*] WINEPREFIX = " + options["WINEPREFIX"] )
 
     # Temp folder
     config += '# Path to temporary directory\n'
     config += 'TEMP_DIR="' + options["TEMP_DIR"] + '"\n\n'
-    print " [*] TEMP_DIR = " + options["TEMP_DIR"]
+    print( " [*] TEMP_DIR = " + options["TEMP_DIR"] )
 
     # Metasploit-Framework
     config += '# The path to the metasploit framework, for example: /usr/share/metasploit-framework/\n'
     config += 'METASPLOIT_PATH="' + options['METASPLOIT_PATH'] + '"\n\n'
-    print " [*] METASPLOIT_PATH = " + options['METASPLOIT_PATH']
+    print( " [*] METASPLOIT_PATH = " + options['METASPLOIT_PATH'] )
 
     # msfvenom
     config += '# The path to msfvenom for shellcode generation purposes\n'
     config += 'MSFVENOM_PATH="' + options["MSFVENOM_PATH"] + '"\n\n'
-    print " [*] MSFVENOM_PATH = " + options["MSFVENOM_PATH"]
+    print( " [*] MSFVENOM_PATH = " + options["MSFVENOM_PATH"] )
 
     # msfvenom
     config += '# Default options to pass to msfvenom for shellcode creation\n'
     config += 'MSFVENOM_OPTIONS="' + options['MSFVENOM_OPTIONS'] + '"\n\n'
-    print " [*] MSFVENOM_OPTIONS = " + options['MSFVENOM_OPTIONS']
+    print( " [*] MSFVENOM_OPTIONS = " + options['MSFVENOM_OPTIONS'] )
 
     # PyInstaller Path
     config += '# The path to pyinstaller, for example: /opt/pyinstaller-2.0/\n'
     config += 'PYINSTALLER_PATH="' + options['PYINSTALLER_PATH'] + '"\n\n'
-    print " [*] PYINSTALLER_PATH = " + options['PYINSTALLER_PATH'] + "\n"
+    print( " [*] PYINSTALLER_PATH = " + options['PYINSTALLER_PATH'] + "\n" )
 
 
     # Veil-Evasion
@@ -87,38 +87,38 @@ def generateConfig(options):
 #
 #################################################
 """
-    print "\n Veil-Evasion Configuration:"
+    print( "\n Veil-Evasion Configuration:" )
 
     # Veil-Evasion path
     config += '# Veil-Evasion install path\n'
     config += 'VEIL_EVASION_PATH="' + options['VEIL_EVASION_PATH'] + '"\n\n'
-    print " [*] VEIL_EVASION_PATH = " + options['VEIL_EVASION_PATH']
+    print( " [*] VEIL_EVASION_PATH = " + options['VEIL_EVASION_PATH'] )
 
     # Payload path
     source_path = os.path.expanduser( options["PAYLOAD_SOURCE_PATH"] )
     config += '# Path to output the source of payloads\n'
     config += 'PAYLOAD_SOURCE_PATH="' + source_path + '"\n\n'
-    print " [*] PAYLOAD_SOURCE_PATH = " + source_path
+    print( " [*] PAYLOAD_SOURCE_PATH = " + source_path )
 
     # Compile path
     compiled_path = os.path.expanduser( options["PAYLOAD_COMPILED_PATH"] )
     config += '# Path to output compiled payloads\n'
     config += 'PAYLOAD_COMPILED_PATH="' + compiled_path +'"\n\n'
-    print " [*] PAYLOAD_COMPILED_PATH = " + compiled_path
+    print( " [*] PAYLOAD_COMPILED_PATH = " + compiled_path )
 
     # Handler path
     handler_path = os.path.expanduser( options["HANDLER_PATH"] )
     config += '# Whether to generate a msf handler script and where to place it\n'
     config += 'GENERATE_HANDLER_SCRIPT="' + options['GENERATE_HANDLER_SCRIPT'] + '"\n'
-    print " [*] GENERATE_HANDLER_SCRIPT = " + options['GENERATE_HANDLER_SCRIPT']
+    print( " [*] GENERATE_HANDLER_SCRIPT = " + options['GENERATE_HANDLER_SCRIPT'] )
     config += 'HANDLER_PATH="' + handler_path + '"\n\n'
-    print " [*] HANDLER_PATH = " + handler_path
+    print( " [*] HANDLER_PATH = " + handler_path )
 
     # Hash List
     hash_path = os.path.expanduser( options["HASH_LIST"] )
     config += '# Running hash list of all payloads generated\n'
     config += 'HASH_LIST="' + hash_path + '"\n\n'
-    print " [*] HASH_LIST = " + hash_path + "\n"
+    print( " [*] HASH_LIST = " + hash_path + "\n" )
 
 
     # Veil-Catapult
@@ -130,39 +130,39 @@ def generateConfig(options):
 #
 #################################################
 """
-    print "\n Veil-Catapult Configuration:"
+    print( "\n Veil-Catapult Configuration:" )
 
     # Veil-Catapult path
     config += '# Veil-Catapult install path\n'
     config += 'VEIL_CATAPULT_PATH="' + options['VEIL_CATAPULT_PATH'] + '"\n\n'
-    print " [*] VEIL_CATAPULT_PATH = " + options['VEIL_CATAPULT_PATH']
+    print( " [*] VEIL_CATAPULT_PATH = " + options['VEIL_CATAPULT_PATH'] )
 
     # Veil-Catapult resource path
     catapult_resource_path = os.path.expanduser( options["CATAPULT_RESOURCE_PATH"] )
     config += '# Path to output Veil-Catapult resource/cleanup files\n'
     config += 'CATAPULT_RESOURCE_PATH="' + catapult_resource_path + '"\n\n'
-    print " [*] CATAPULT_RESOURCE_PATH = " + catapult_resource_path + "\n"
+    print( " [*] CATAPULT_RESOURCE_PATH = " + catapult_resource_path + "\n" )
 
 
     # Create the output compiled path if it doesn't exist
     if not os.path.exists( handler_path ):
         os.makedirs( handler_path )
-        print " [I] Path Created: '" + handler_path
+        print( " [I] Path Created: '" + handler_path )
 
     # Create the catapult resource path if it doesn't exist
     if not os.path.exists( catapult_resource_path ):
         os.makedirs( catapult_resource_path )
-        print " [I] Path Created: '" + catapult_resource_path
+        print( " [I] Path Created: '" + catapult_resource_path )
 
     # Create the output source path if it doesn't exist
     if not os.path.exists( source_path ):
         os.makedirs( source_path )
-        print " [I] Path Created: '" + source_path
+        print( " [I] Path Created: '" + source_path )
 
     # Create the output compiled path if it doesn't exist
     if not os.path.exists( compiled_path ):
         os.makedirs( compiled_path )
-        print " [I] Path Created: '" + compiled_path
+        print( " [I] Path Created: '" + compiled_path )
 
 
     # Save config
@@ -170,14 +170,14 @@ def generateConfig(options):
         # create the output compiled path if it doesn't exist
         if not os.path.exists( "/etc/veil/" ):
             os.makedirs( "/etc/veil/" )
-            print " [I] Path '/etc/veil/' Created"
+            print( " [I] Path '/etc/veil/' Created" )
         f = open( "/etc/veil/settings.py", 'w' )
         f.write( config )
         f.close()
-        os.chmod( "/etc/veil/settings.py", 0755 )
-        print " [I] Configuration File Written To: '/etc/veil/settings.py'\n"
+        os.chmod( "/etc/veil/settings.py", 0o0755 )
+        print( " [I] Configuration File Written To: '/etc/veil/settings.py'\n" )
     else:
-        print " [!] ERROR: PLATFORM NOT CURRENTLY SUPPORTED"
+        print( " [!] ERROR: PLATFORM NOT CURRENTLY SUPPORTED" )
         sys.exit()
 
 
@@ -187,7 +187,7 @@ if __name__ == '__main__':
 
     # Check for root access
     if os.geteuid() != 0:
-        print " [!] ERROR: Not root. Requesting..."
+        print( " [!] ERROR: Not root. Requesting..." )
         os.execvp( "sudo", ["sudo"] + ["python"] + sys.argv )
         sys.exit()
 
@@ -244,7 +244,7 @@ if __name__ == '__main__':
 
     # Unsupported platform...
     else:
-        print " [!] ERROR: PLATFORM NOT CURRENTLY SUPPORTED"
+        print( " [!] ERROR: PLATFORM NOT CURRENTLY SUPPORTED" )
         sys.exit()
 
     generateConfig( options )
