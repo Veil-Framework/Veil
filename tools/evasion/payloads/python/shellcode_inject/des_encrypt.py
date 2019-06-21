ced31c98fc44b6e183dcf23d3d2986d45b7fbc11
Seb <sebmora1055@gmail.com>
2019-06-20 22:20:05 -0700

    removed unused imports

--- tools/evasion/payloads/python/shellcode_inject/des_encrypt.py
+++ tools/evasion/payloads/python/shellcode_inject/des_encrypt.py
@@ -8,8 +8,6 @@ Great examples and code adapted from:
 Module built by @christruncer
 """
 
-from datetime import date
-from datetime import timedelta
 from tools.evasion.evasion_common import encryption
 from tools.evasion.evasion_common import evasion_helpers
 from tools.evasion.evasion_common import gamemaker
