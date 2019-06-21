c5ff52fed5611908b6a8fd714694d8e5f3efb007
Seb <sebmora1055@gmail.com>
2019-06-20 22:19:17 -0700

    removed unused imports

--- tools/evasion/payloads/python/shellcode_inject/base64_substitution.py
+++ tools/evasion/payloads/python/shellcode_inject/base64_substitution.py
@@ -7,9 +7,6 @@ Module built by @christruncer
 
 import base64
 
-from datetime import date
-from datetime import timedelta
-
 from tools.evasion.evasion_common import evasion_helpers
 from tools.evasion.evasion_common import encryption
 from tools.evasion.evasion_common import gamemaker
